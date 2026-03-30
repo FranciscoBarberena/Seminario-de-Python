606ac9cc99ad223070a6acf8d2f2f89fc8efe083
Francisco Barberena <francisco.barberena275991@alumnos.info.unlp.edu.ar>
2026-03-30 12:28:49 -0300

    Ej3 añadido a notebook

--- Prácticas/Practica 2/Entrega 2/src/Ejercicio3.py
+++ Prácticas/Practica 2/Entrega 2/src/Ejercicio3.py
@@ -1,27 +1,27 @@
-review = """La película sigue a un grupo de astronautas que
-viajan a Marte
-en una misión de rescate. El capitán Torres lidera al equipo
-a través
-de tormentas solares y fallos en el sistema de navegación. Al
-llegar
-a Marte descubren que la base está abandonada y los
-suministros
-destruidos. Torres decide sacrificar la nave nodriza para
-salvar
-al equipo y logran volver a la Tierra en una cápsula de
-emergencia.
-El final revela que Torres sobrevivió gracias a un pasaje
-secreto."""
 
+def execute3():
+    review = """La película sigue a un grupo de astronautas que
+    viajan a Marte
+    en una misión de rescate. El capitán Torres lidera al equipo
+    a través
+    de tormentas solares y fallos en el sistema de navegación. Al
+    llegar
+    a Marte descubren que la base está abandonada y los
+    suministros
+    destruidos. Torres decide sacrificar la nave nodriza para
+    salvar
+    al equipo y logran volver a la Tierra en una cápsula de
+    emergencia.
+    El final revela que Torres sobrevivió gracias a un pasaje
+    secreto."""
 
-
-listaPalabrasSpoiler= (input("Ingrese las palabras spoiler (separadas por coma): ")).lower().split(",")
-listaPalabrasSpoiler = [spoiler.strip() for spoiler in listaPalabrasSpoiler]
-listaCompletaDePalabras = review.split()
-reviewSinSpoilers=""
-for palabra in listaCompletaDePalabras:
-    if palabra.lower() in listaPalabrasSpoiler:
-        reviewSinSpoilers += f"{len(palabra)*"*"} "
-    else:
-        reviewSinSpoilers+= f"{palabra} "
-print(reviewSinSpoilers)
+    listaPalabrasSpoiler= (input("Ingrese las palabras spoiler (separadas por coma): ")).lower().split(",")
+    listaPalabrasSpoiler = [spoiler.strip() for spoiler in listaPalabrasSpoiler]
+    listaCompletaDePalabras = review.split()
+    reviewSinSpoilers=""
+    for palabra in listaCompletaDePalabras:
+        if palabra.lower() in listaPalabrasSpoiler:
+            reviewSinSpoilers += f"{len(palabra)*"*"} "
+        else:
+            reviewSinSpoilers+= f"{palabra} "
+    print(reviewSinSpoilers)
